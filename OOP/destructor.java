6febc19bc11d93a942f5a7c81a902e2b82bdba76
Shoyeb45 <shoyebff45@gmail.com>
2024-05-12 09:07:03 +0530

    Destructor

---
+++ OOP/destructor.java
@@ -0,0 +1,6 @@
+public class destructor {
+    public static void main(String[] args) {
+        StringBuilder a = new StringBuilder("We don't need to write destructor for objects in java. Java will by-default destroy the objects by garbage collector.");
+        System.out.println(a);
+    }
+}
