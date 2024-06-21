30987f056bf050a5718c81dbf161e773b85a0cff
Shoyeb45 <shoyebff45@gmail.com>
2024-06-21 16:48:52 +0530

    Hashmap implementaion

--- HashMap/HashMap.java
+++ HashMap/HashMap.java
@@ -1,6 +1,7 @@
 package HashMap;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 
 /**
@@ -9,6 +10,7 @@ import java.util.ArrayList;
  */
 public class HashMap<K, V> {
     public static final int MAX_SIZE = 16;
+    public static final double MAX_THRESHOLD = 0.5;
 
     private class Entry<K, V> {
         K key;
@@ -36,15 +38,85 @@ public class HashMap<K, V> {
     }
     
     private int hash(K key) {
-        return key.hashCode() % capacity;
+        return key.hashCode();
     }
-    public void insert(K k, V v) {
-        int idx = hash(k);
-        Entry newEntry = new Entry<K,V>(k, v, idx);
-        for(int i = 0; i < data.get(idx).size(); i++) {
-            if(data.get(idx).get(i).key == k) {
-                // data.get(idx).get(i) = new_entry;
+
+    private int getIndex(int hc) {
+        return (hc % capacity + capacity) % capacity; 
+    }
+
+    private void resize() {
+        int new_capacity = 2 * capacity;
+
+        ArrayList<ArrayList<Entry<K, V>>> new_data = new ArrayList<>();
+        for(int i = 0; i < new_capacity; i++) {
+            new_data.add(new ArrayList<Entry<K, V>>());
+        }
+
+        for(int i = 0; i < capacity; i++) {
+            for(Entry<K, V> entry: data.get(i)) {
+                int new_index = getIndex(entry.hashCode % new_capacity);
+                new_data.get(new_index).add(entry);
             }
         }
+        data = new_data;
+        capacity = new_capacity;
     }
+
+    public void insert(K k, V v) {
+        if((double) sz / capacity > MAX_THRESHOLD) {
+            this.resize();
+        }
+
+        int idx = getIndex(hash(k));
+        Entry<K, V> newEntry = new Entry<K,V>(k, v, idx);
+        
+        for(Entry<K, V> entry: data.get(idx)) {
+            if(entry.key == k) {
+                entry.value = v;
+                return;
+            }
+        }
+        data.get(idx).add(newEntry);
+        sz++;   
+    }
+
+    public int size() {
+        return sz;
+    }
+
+    boolean empty() {
+        return (sz == 0);
+    }
+
+    boolean contains(K key) {
+        int idx = getIndex(hash(key));
+        for(int i = 0; i < data.get(i).size(); i++) {
+            if(data.get(idx).get(i).key == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void delKey(K key) {
+        int idx = getIndex(hash(key));
+        for(int i = 0; i < data.get(idx).size(); i++) {
+            if(data.get(idx).get(i).key == key) {
+                data.get(idx).remove(i);
+            }
+        }
+        throw new NoSuchElementException("Key not found: " + key);
+    }
+
+    V get(K key) {
+        int idx = getIndex(hash(key));
+        for(int i = 0; i < data.get(idx).size(); i++) {
+            if(data.get(idx).get(i).key == key) {
+                return data.get(idx).get(i).value;
+            }
+        }
+        return null;
+    }
+    
 }
