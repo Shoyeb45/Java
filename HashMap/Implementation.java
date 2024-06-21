e1406bf5083ab7b62076389483a8101d1cdb4d56
Shoyeb45 <shoyebff45@gmail.com>
2024-06-21 17:51:04 +0530

    Hash Map implementation using arraylist

--- HashMap/Implementation.java
+++ HashMap/Implementation.java
@@ -1,11 +1,27 @@
 package HashMap;
-// import HashMap.HashMap;
 import java.util.Scanner;
 
 public class Implementation {
     public static void main(String[] args) {
         Scanner sc = new  Scanner(System.in);
-        // HashMap hm = new HashMap<Float, Integer>();
+        HashMap<String, Integer> mp = new HashMap<>();
+        mp.insert("Stud1", 19);
+        mp.insert("Stud2", 18);
+        System.out.println("Size " + mp.size());
+        System.out.println(mp.get("Stud1"));
+        System.out.println(mp.get("stud1"));  
+
+        mp.delKey("Stud1");
+        System.out.println("Size " + mp.size());
+        System.out.println("Empty: " + mp.empty());
+        
+        mp.insert("Stud3", 21);
+        System.out.println(mp.contains("Stud3"));
+        System.out.println("Size " + mp.size());
+        mp.delKey("Stud3");
+        mp.delKey("Stud2");
+        System.out.println("Empty: " + mp.empty());
+        
         sc.close();
     
     }
