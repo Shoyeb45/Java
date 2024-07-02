faa2e098eaa2a9a938a9f7f811e73af1b0d613b7
Shoyeb45 <shoyebff45@gmail.com>
2024-07-02 14:19:53 +0530

    Real number data type

---
+++ JavaBasic1/RealNumber.java
@@ -0,0 +1,10 @@
+public class RealNumber {
+    public static void main(String[] args) {
+        double a = 0.06;
+        double b = 0.04;
+        System.out.println(a - b);        
+        System.out.println(a + b);        
+        System.out.println(a * b);        
+        System.out.println(a / b);        
+    }
+}
