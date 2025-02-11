e7a93ed10e683199a23492885f7f50e7e89a5d8f
Shoyeb45 <shoyebff45@gmail.com>
2025-02-11 09:25:10 +0530

    Javafx Practice files moved

---
+++ array/ObjectsOfArray.java
@@ -0,0 +1,13 @@
+package array;
+
+class Fan {
+    String brand;
+    int noOfBlades;
+    float price;
+}
+
+public class ObjectsOfArray {
+    public static void main(String[] args) {
+        
+    }
+}
